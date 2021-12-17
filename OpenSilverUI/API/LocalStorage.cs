626be38086d96007a749ca9dbf317ed22ba6441a
kikipoulet <sepulchreclement.dic@gmail.com>
2021-12-17 10:29:57 +0100

    Add files via upload

---
+++ OpenSilverUI/API/LocalStorage.cs
@@ -0,0 +1,20 @@
+﻿using OpenSilver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenSilverApplication3
+{
+    public static class LocalStorage
+    {
+        public static void Set(string key, string value)
+        {
+            Interop.ExecuteJavaScript($"localStorage.setItem('{key}', '{value}');");
+        }
+
+        public static string Get(string key)
+        {
+            return Interop.ExecuteJavaScript($"localStorage.getItem('{key}');").ToString();
+        }
+    }
+}
---
+++ OpenSilverUI/MessageWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace OpenSilverUI
+{
+    public partial class MessageWindow : ChildWindow
+    {
+        public MessageWindow(string Title, string Message)
+        {
+            InitializeComponent();
+            TextBlockMessage.Text = Message;
+            TextBlockTitle.Text = Title;
+        }
+
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+    
+    }
+}
+
